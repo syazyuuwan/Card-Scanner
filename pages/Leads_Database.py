509ada1d7a6b26313433937a3af69010882d2aa8
syazyuuwan <nawsay7@gmail.com>
2024-08-27 15:42:50 +0800

    Update Leads_Database.py

--- pages/Leads_Database.py
+++ pages/Leads_Database.py
@@ -7,4 +7,4 @@ if st.button("Refresh"):
     data_df = pd.read_csv("leads.csv")
 
 if data_df is not None:
-    st.dataframe(data_df, hide_index=True, use_container_width=True)
+    st.data_editor(data_df, hide_index=True, use_container_width=True,num_rows="dynamic")
